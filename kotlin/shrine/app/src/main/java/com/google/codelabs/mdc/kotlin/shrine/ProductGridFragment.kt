64fa0e69f8ae2d0af31df2e8c78bb115525ee5c8
Praveen <pkchmits@gmail.com>
2022-02-10 16:58:34 +0530

    Added backdrop to list

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,12 +1,15 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,8 +29,20 @@ class ProductGridFragment : Fragment() {
         // Inflate the layout for this fragment with the ProductGrid theme
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
 
+
+        // Set cut corner background for API 23+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            view.product_grid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
+
         // Set up the tool bar
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+
+        view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(activity!!,view.product_grid
+            ,AccelerateDecelerateInterpolator(),
+            ContextCompat.getDrawable(context!!, R.drawable.shr_branded_menu), // Menu open icon
+            ContextCompat.getDrawable(context!!, R.drawable.shr_close_menu) // Menu close icon
+        ))
 
         // Set up the RecyclerView
         view.recycler_view.setHasFixedSize(true)
